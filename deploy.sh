5810d7b578945ac6b11e70853059745fdc973ee6
liyongleihf2006 <573044965@qq.com>
2020-02-25 16:33:44 +0800

    再测试一次

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:liyongleihf2006/liyongleihf2006.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
